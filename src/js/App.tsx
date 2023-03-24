d52e094cb164457a49a3d1f4b76d0fec2cf47390
aranpect <120459638+aranpect@users.noreply.github.com>
2023-03-24 15:24:13 +0900

    base

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -3,7 +3,7 @@ import Twitter from "../image/twitter.webp";
 import Github from "../image/github.webp";
 import Discord from "../image/discord.webp";
 import Logo from "../image/logo.webp";
-import { MobileView, BrowserView } from "react-device-detect";
+import { MobileView, BrowserView ,isMobile} from "react-device-detect";
 
 export default function App() {
   return (
@@ -12,20 +12,20 @@ export default function App() {
       <div className="glass">
       <main className="relative">
         <div className="flex bg-green-200">
-          <div className="w-4/6 flex">
-            <img src={Logo} alt="Logo" className="logo-fix"></img>
+          <div className="w-4/6 flex mobile-top">
+            <a href="https://aranpect.com"><img src={Logo} alt="Logo" className="logo-fix"></img></a>
             <p className="w-1/6 p-3 m-1 flex logo not-large"><a href="https://aranpect.com">Aranpect</a></p>
           </div>
-          <div className="btn-top w-2/6">
-            <p className="p-3 m-1 p-header justify-around btn-top">お問い合わせ</p>
-            <p className="p-3 m-1 p-header justify-around btn-top"><a href="https://github.com/aranpect">開発物</a></p>
-            <p className="p-3 m-1 p-header justify-around btn-top">ホーム</p>
+          <div className="btn-top w-2/6 mobile-top-right">
+            <p className="p-3 m-1 p-header justify-around btn-top mobile-top-p">お問い合わせ</p>
+            <p className="p-3 m-1 p-header justify-around btn-top mobile-top-p"><a href="https://github.com/aranpect">開発物</a></p>
+            <p className="p-3 m-1 p-header justify-around btn-top mobile-top-p">ホーム</p>
           </div>
         </div>
         <section>
           <div className="flex">
-            <div className="w-1/5 border-2 rounded-md p-3 m-1 bg-violet-600 div-up">
-              <p className="text-center text-3xl text-white">LINK</p>
+            <div className="w-1/5 border-2 rounded-md p-3 m-1 bg-violet-600 div-up mobile-div">
+              <p className="text-center text-3xl text-white mobile-p">LINK</p>
               <a href="https://twitter.com/aranpect"><img src={Twitter} alt="twitter" className="img-toto"></img></a>
               <p className="text-center text-white">Twitter</p>
               <a href="https://discord.com"><img src={Discord} alt="discord" className="img-toto"></img></a>
@@ -33,23 +33,23 @@ export default function App() {
               <a href="https://github.com/aranpect"><img src={Github} alt="github" className="img-toto"></img></a>
               <p className="text-center text-white">Github</p>
             </div>
-            <div className="w-3/5 border-2 rounded-md p-3 m-1 bg-blue-600 div-up">
+            <div className="w-3/5 border-2 rounded-md p-3 m-1 bg-blue-600 div-up mobile-div">
               <h1 className="text-center text-white p-3">Aranpect</h1>
             </div>
-            <div className="w-1/5 border-2 rounded-md p-3 m-1 bg-red-600 div-up">
-              <p className="text-center text-3xl text-white">Service</p>
+            <div className="w-1/5 border-2 rounded-md p-3 m-1 bg-red-600 div-up mobile-div">
+              <p className="text-center text-3xl text-white mobile-p">Service</p>
             </div>
           </div>
           <div className="flex">
             <div className="w-1/5">
               
             </div>
-            <div className="w-3/5 justify-center p-3 border-black go-white rounded-md div-up">
+            <div className="w-3/5 justify-center p-3 border-black go-white rounded-md div-up mobile-div">
               <p className="main-p border-2 rounded-md bg-green-100 text-3xl p-2">自己紹介</p>
               <div>
-                <p className="text-center">ガジェットが好きな学生</p>
-                <p className="text-center">たまにプログラミングをしています</p>
-                <p className="text-center">よろしくおねがいします！</p>
+                <p className="text-center under-p">ガジェットが好きな学生</p>
+                <p className="text-center under-p">たまにプログラミングをしています</p>
+                <p className="text-center under-p">よろしくおねがいします！</p>
               </div>
             </div>
             <div className="w-1/5">
@@ -60,12 +60,13 @@ export default function App() {
             <div className="w-1/5">
               <p className="text-center">広告枠</p>
             </div>
-            <div className="w-3/5 justify-center p-3 border-black go-white rounded-md div-up">
+            <div className="w-3/5 justify-center p-3 border-black go-white rounded-md div-up mobile-div">
               <p className="main-p border-2 rounded-md bg-green-100 text-3xl p-2">相互リンク</p>
               <div>
                 <a href="https://takoserver.com"><p className="text-center a-large">takoserver</p></a>
                 <a href="https://suzuneu.com"><p className="text-center a-large">すずねーう</p></a>
                 <a href="https://natsukimc.com"><p className="text-center a-large">なつきmc</p></a>
+                <a href="https://404kit.com"><p className="text-center a-large">404kit</p></a>
               </div>
             </div>
             <div className="w-1/5">
